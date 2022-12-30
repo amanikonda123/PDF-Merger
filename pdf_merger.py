e49c415f06cf665fd387f586524082d6cb6249fd
Aditya Manikonda <adityamanikonda123@gmail.com>
2022-12-30 01:02:14 -0800

    done v3

--- pdf_merger.py
+++ pdf_merger.py
@@ -3,17 +3,18 @@ import sys, os
 
 merger = PdfMerger()
 
-for item in sys.argv:
-    print(item)
-
 if len(sys.argv) < 3:
-    sys.exit("usage: python pdf_merger.py [NEW_FILE] [MERGED_FILES]")
+    sys.exit("usage: python pdf_merger.py [NEW_FILE] [INPUT_FILES]")
 
 files = []
+msg = "\nMerging: \n"
 for i in range(2, len(sys.argv)):
     files.append("/Users/Adiman17/Downloads/" + sys.argv[i])
+    msg += sys.argv[i] + " \n"
+print(msg + " ...")
 
 for file in files:
     if file.endswith(".pdf"):
         merger.append(file)
     merger.write(sys.argv[1])
+print("Done merging into: " + sys.argv[1] + ".\n")
